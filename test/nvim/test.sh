bf50abe5aa31be4391414a0d279168baa4c10a6a
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-09-24 11:08:41 +0200

    fix(test)

--- test/nvim/test.sh
+++ test/nvim/test.sh
@@ -40,7 +40,7 @@ source dev-container-features-test-lib
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib. Syntax is...
 # check <LABEL> <cmd> [args...]
-check "execute command" bash -c "nvim --version | grep 'NVIM v0.11.0'"
+check "check nvim execution" bash -c "nvim --version"
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
